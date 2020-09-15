62944df1b287b240dee9f0f97ecc574d8aaf57c5
Nguyễn Khắc Qui <hivongmotloihua@gmail.com>
2020-09-15 16:36:57 +0700

    Test các phạm vi truy cập

---
+++ src/Circle.java
@@ -0,0 +1,29 @@
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    private void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(3.0);
+        System.out.println(circle.getRadius());
+
+    }
+}
